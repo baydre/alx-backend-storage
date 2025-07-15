360a4a0960c4f1073f9c31271fd2ac50aed8e24d
baydre_africa <baydreafrica@gmail.com>
2025-07-15 13:53:28 +0100

    chore: reading from redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """
-Task 0: Writing strings to Redis.
+Task 0: Writing strings to Redis (ln 1-32).
+Task 1: Reading from Redis and recovering
+original type (ln 34-57).
 """
 import redis
 import uuid
@@ -28,3 +30,28 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> \
+            Union[str, bytes, int, float, None]:
+        """
+        Retrieves data from Redis and optionally converts it
+        using a callable If the key does not exist, returns None.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """
+        Retrieve data from Redis as a UTF-8 string.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """
+        Retrieves data from Redis as an integer.
+        """
+        return self.get(key, fn=int)
