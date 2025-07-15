09a75defc51560e6b1bb10d508952c6c73f0f51b
baydre_africa <baydreafrica@gmail.com>
2025-07-15 14:15:59 +0100

    chore: incrementing valuesw

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,10 +2,12 @@
 """
 Task 0: Writing strings to Redis (ln 1-32).
 Task 1: Reading from Redis and recovering
-original type (ln 34-57).
+original type (ln 36-59).
+Task 2: Incrementing values (ln 61-86).
 """
 import redis
 import uuid
+import functools
 from typing import Union, Callable, Any
 
 
@@ -55,3 +57,30 @@ class Cache:
         Retrieves data from Redis as an integer.
         """
         return self.get(key, fn=int)
+
+    def count_calls(method: Callable) -> Callable:
+        """
+        Decorator to count how many times a method is called.
+        Uses the method's qualified name as a Redis key for
+        the counter.
+        """
+        @functools.wraps(method)
+        def wrapper(self, *args, **kwargs) -> Any:
+            """
+            Wrapper function that increments the call count
+            and executes the original method.
+            """
+            self._redis.incr(method.__qualname__)
+            return method(self, *args, **kwargs)
+        return wrapper
+
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Store the input data in Redis using a random key and
+        returns the key. The data can be a string, bytes, integer,
+        or float. This methods's calls are counted.
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
